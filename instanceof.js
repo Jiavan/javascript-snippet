93250607b6769d94c5d1d06127a5375b51e2d94b
jiavan <jiavan.com@gmail.com>
2019-01-21 10:50:12 +0800

    add instanceof

---
+++ instanceof.js
@@ -0,0 +1,16 @@
+function instanceOf(left, right) {
+    var left = left.__proto__,
+        right = right.prototype;
+
+    while (true) {
+        if (left === null) {
+            return false;
+        }
+
+        if (left === right) {
+            return true;
+        }
+
+        left = left.__proto__;
+    }
+}
